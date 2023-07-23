588bbfcab80dc50b444b738d1f63cb1969d21756
kohrd <konrad.janeczek@protonmail.com>
2023-07-23 23:20:40 +0200

    69

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 import 'answer_button.dart';
 
@@ -16,21 +17,28 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(context) {
+    final currentQuestionFromList = questions[0];
+
+
     return  SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text("The question...",
-              style: TextStyle(
-                  color: Colors.white60,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 25)),
+           Padding(
+             padding: const EdgeInsets.symmetric(horizontal: 25.0),
+             child: Text(currentQuestionFromList.text,
+                style: const TextStyle(
+
+                    color: Colors.white60,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25)),
+           ),
           const SizedBox(height: 30),
-          AnswerButton(answerText: 'answer 1', onTap: (){}),
-          AnswerButton(answerText: 'answer 2', onTap: (){}),
-          AnswerButton(answerText: 'answer 3', onTap: (){}),
-          AnswerButton(answerText: 'answer 4', onTap: (){}),
+          AnswerButton(answerText: currentQuestionFromList.answers[0], onTap: (){}),
+          AnswerButton(answerText: currentQuestionFromList.answers[1], onTap: (){}),
+          AnswerButton(answerText: currentQuestionFromList.answers[2], onTap: (){}),
+          AnswerButton(answerText: currentQuestionFromList.answers[3], onTap: (){}),
 
         ],
       ),
