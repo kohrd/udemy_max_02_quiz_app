79addab4e6c54638b99707b828815638dfcdef15
kohrd <konrad.janeczek@protonmail.com>
2023-07-23 21:13:18 +0200

    63

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:quiz_app/questions_screen.dart';
 import 'package:quiz_app/start_screen.dart';
@@ -8,13 +7,12 @@ class Quiz extends StatefulWidget {
 
   @override
   State<Quiz> createState() {
-  return _QuizState();
+    return _QuizState();
   }
 }
 
 
-
-class _QuizState extends State<Quiz>{
+class _QuizState extends State<Quiz> {
   var activeScreen = 'start-screen';
 
   // @override
@@ -24,7 +22,7 @@ class _QuizState extends State<Quiz>{
   // }
 
 
-  void switchScreen(){
+  void switchScreen() {
     setState(() {
       activeScreen = 'question-screen';
     });
@@ -32,25 +30,30 @@ class _QuizState extends State<Quiz>{
 
 
   @override
-  Widget build( context) {
-    return   MaterialApp(
+  Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'question-screen') {
+      screenWidget = const QuestionScreen();
+    };
+
+    return MaterialApp(
       home: Scaffold(
         body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                colors: [
-                  // Color.fromARGB(a, r, g, b),
-                  Color.fromARGB(255, 55, 93, 119),
-                  Color.fromARGB(105, 24, 36, 110)],
-                begin: Alignment.bottomLeft,
-                end: Alignment.topRight
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [
+                    // Color.fromARGB(a, r, g, b),
+                    Color.fromARGB(255, 55, 93, 119),
+                    Color.fromARGB(105, 24, 36, 110)],
+                  begin: Alignment.bottomLeft,
+                  end: Alignment.topRight
+              ),
             ),
-          ),
-          child:  activeScreen == 'start-screen' ? StartScreen(switchScreen): const QuestionScreen(),
+            child: screenWidget,
         ),
       ),
     );
-
   }
 
 }
